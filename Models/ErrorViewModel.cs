399bf4bcd1c9ee34964ef770c5cb0ba83f8638ec
Yasin <Yasin@DESKTOP-7U76TK6>
2023-06-15 22:09:04 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KutuphaneProjesı.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
