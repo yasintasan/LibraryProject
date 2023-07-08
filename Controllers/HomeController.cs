dd8c3dfc82745d404e10e429b80042a6ded4927b
Yasin <Yasin@DESKTOP-7U76TK6>
2023-07-08 23:13:15 +0300

    ff

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,23 @@
-﻿using KutuphaneProjesı.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace KutuphaneProjesı.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
+        public IActionResult Book()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Categoory()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Student()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
